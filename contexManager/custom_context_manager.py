e5197927e1ef571abd90a208f2996edc4b25430f
sohan8261231 <sohan@ariksa.com>
2025-01-08 13:33:56 +0530

    Revert "custom context manager"
    This reverts commit f2252579a47d2006ed83252ad9ea75fd4ca02ab2.

--- contexManager/custom_context_manager.py
+++ contexManager/custom_context_manager.py
@@ -1,7 +1,58 @@
-"""Write a function to find the longest common prefix string amongst an array of strings.
 
-If there is no common prefix, return an empty string "".
-"""
+# import time
 
 
-def 
+# class Timer:
+#    def __enter__(self):
+#        self.start_time = time.time()
+#        return self
+
+#    def __exit__(self, exc_type, exc_value, traceback):
+#        self.end_time = time.time()
+#        elapsed_time = self.end_time - self.start_time
+#        print(f"Elapsed time: {elapsed_time} seconds")
+
+
+# # Example usage
+# if __name__ == "__main__":
+#    with Timer() as timer:
+#        # Code block to measure the execution time
+#        time.sleep(2)  # Simulate some time-consuming operation
+# Elapsed time: 2.002082347869873 seconds
+
+
+import time 
+from contextlib import contextmanager
+
+class Timer:
+
+    def __enter__(self):
+        self.start_time = time.time() 
+        return self 
+    
+    def __exit__(self,exc_type,exc_value,traceback):
+        self.end_time = time.time()
+        elasped_time = self.end_time - self.start_time
+        print(f"Elapsed time: {elasped_time} seconds")
+
+    def how(self):
+        print("Hello")
+
+
+@contextmanager    
+def timer():
+    start_time = time.time()
+    yield # Logic of __enter__ before yield and __exit__ after yield
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print(f"Elapsed time: {elapsed_time} seconds")
+
+
+
+if __name__ == "__main__":
+    with Timer() as t:
+        time.sleep(2) # Simulate some time-consuming operation
+        t.how()
+    
+    with timer():   
+        time.sleep(2)
